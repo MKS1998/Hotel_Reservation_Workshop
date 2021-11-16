bc7d6cd0d59faf7f533dc01a47a73c2194e702ef
MKS1998 <nksayyad2900@outlook.com>
2021-11-16 17:36:55 +0530

    Added : Program to Add Rating for the hotel reservation system

--- src/test/java/com/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/hotelreservation/HotelReservationTest.java
@@ -9,11 +9,11 @@ public class HotelReservationTest {
 	@Test
 	public void whenAddedHotel_ShouldReturn_AddedHotel() {
 		try {
-			boolean isValid1 = hotelReservation.addHotel(new Hotel("LakeWood", 100, 80));
+			boolean isValid1 = hotelReservation.addHotel(new Hotel("LakeWood", "Regular", 110, 90, 3));
 			Assert.assertTrue(isValid1);
-			boolean isValid2 = hotelReservation.addHotel(new Hotel("Bridgewood", 200, 100));
+			boolean isValid2 = hotelReservation.addHotel(new Hotel("Bridgewood", "Regular", 160, 50, 4));
 			Assert.assertTrue(isValid2);
-			boolean isValid3 = hotelReservation.addHotel(new Hotel("Ridgewood", 300, 120));
+			boolean isValid3 = hotelReservation.addHotel(new Hotel("Ridgewood", "Regular", 220, 150, 5));
 			Assert.assertTrue(isValid3);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -24,5 +24,11 @@ public class HotelReservationTest {
 	public void toFindTheChepeastHotel() {
 		HotelReservationSystem cheapHotel = new HotelReservationSystem();
 		cheapHotel.findCheapestHotel();
+	}
+
+	@Test
+	public void toFindTheCheapestHotel_OnWeekdayAndWeekend() {
+		HotelReservationSystem cheapHotel = new HotelReservationSystem();
+		cheapHotel.findCheapestHotel_Weekday_Weekend();
 	}
 }
